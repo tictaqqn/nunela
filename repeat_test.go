5d0640928447b1cefa80fb4b14d1585c73a9f569
bababax11 <38175513+bababax11@users.noreply.github.com>
2022-03-16 22:15:33 +0900

    test: add repeat tests

--- repeat_test.go
+++ repeat_test.go
@@ -20,6 +20,15 @@ func TestRepeat(t *testing.T) {
 			[]int{2, 3, 2},
 			nune.FromBuffer([]int{0, 0, 2, 2, 4, 4, 1, 1, 3, 3, 5, 5}).Reshape(2, 3, 2),
 		},
+		"3D tensor": {
+			nune.Range[int](1, 31, 1).Reshape(5, 3, 2),
+			[]int{3, 1, 2},
+			[]int{2, 3, 2, 5},
+			nune.FromBuffer([]int{
+				1, 7, 13, 19, 25, 2, 8, 14, 20, 26, 3, 9, 15, 21, 27, 4, 10, 16, 22, 28, 5, 11, 17, 23, 29, 6, 12, 18, 24, 30,
+				1, 7, 13, 19, 25, 2, 8, 14, 20, 26, 3, 9, 15, 21, 27, 4, 10, 16, 22, 28, 5, 11, 17, 23, 29, 6, 12, 18, 24, 30,
+			}).Reshape(2, 3, 2, 5),
+		},
 	}
 	for name, tt := range cases {
 		t.Run(name, func(t *testing.T) {
