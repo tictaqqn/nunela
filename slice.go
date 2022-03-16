15f7189820163e404fd574849cf7efe9ac896f7c
bababax11 <38175513+bababax11@users.noreply.github.com>
2022-03-16 17:54:56 +0900

    feat: add slice functions

---
+++ slice.go
@@ -0,0 +1,18 @@
+package nunela
+
+func every[T any](s []T, f func(int, T) bool) bool {
+	for i, v := range s {
+		if !f(i, v) {
+			return false
+		}
+	}
+	return true
+}
+
+func mapSlice[T any, U any](sl []T, f func(T) U) []U {
+	out := make([]U, 0, len(sl))
+	for _, v := range sl {
+		out = append(out, f(v))
+	}
+	return out
+}
