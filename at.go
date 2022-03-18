333159456028328394e9e818c317971a0502fb3c
bababax11 <38175513+bababax11@users.noreply.github.com>
2022-03-18 16:09:02 +0900

    docs: add documentations

--- at.go
+++ at.go
@@ -2,6 +2,7 @@ package nunela
 
 import "github.com/vorduin/nune"
 
+// TryGetIndex returns the index of data of tensor at the given indices.
 func TryGetIndex[T Number](tensor *nune.Tensor[T], indices ...int) (int, error) {
 	if tensor.Rank() != len(indices) {
 		return 0, NewErrDifferentRankAndIndices(tensor, indices)
@@ -16,6 +17,7 @@ func TryGetIndex[T Number](tensor *nune.Tensor[T], indices ...int) (int, error) 
 	return index, nil
 }
 
+// GetIndex returns the index of data of tensor at the given indices.
 func GetIndex[T Number](tensor *nune.Tensor[T], indices ...int) int {
 	index, err := TryGetIndex(tensor, indices...)
 	if err != nil {
@@ -24,6 +26,7 @@ func GetIndex[T Number](tensor *nune.Tensor[T], indices ...int) int {
 	return index
 }
 
+// TryAt returns the value at the given indices.
 func TryAt[T Number](tensor *nune.Tensor[T], indices ...int) (T, error) {
 	index, err := TryGetIndex(tensor, indices...)
 	if err != nil {
@@ -32,6 +35,7 @@ func TryAt[T Number](tensor *nune.Tensor[T], indices ...int) (T, error) {
 	return tensor.Ravel()[index], nil
 }
 
+// At returns the value at the given indices.
 func At[T Number](tensor *nune.Tensor[T], indices ...int) T {
 	value, err := TryAt(tensor, indices...)
 	if err != nil {
