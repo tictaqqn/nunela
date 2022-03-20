080f90ad2134962325c2a71a67a98dee0df6a2d5
bababax11 <38175513+bababax11@users.noreply.github.com>
2022-03-20 09:48:03 +0900

    perf: less allocation

--- mul.go
+++ mul.go
@@ -218,12 +218,14 @@ func tryStrassenDot[T Number](a *nune.Tensor[T], b *nune.Tensor[T], aAxis int, b
 
 	wg.Add(2)
 	go func(q01, q3, q5 *nune.Tensor[T], a0 **nune.Tensor[T]) {
-		*a0 = Sub(q01, q3)
+		SubAssign(q01, q3)
+		*a0 = q01
 		AddAssign(*a0, q5)
 		wg.Done()
 	}(q01, ts[3], ts[5], &as[0])
 	go func(q24, q1, q6 *nune.Tensor[T], a3 **nune.Tensor[T]) {
-		*a3 = Add(q24, q1)
+		AddAssign(q24, q1)
+		*a3 = q24
 		SubAssign(*a3, q6)
 		wg.Done()
 	}(q24, ts[1], ts[6], &as[3])
